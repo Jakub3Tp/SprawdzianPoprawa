d50800b7c46e14ca7026be7eb9b6e03dc8378121
Student1 <student.student@gmail.com>
2024-04-10 12:22:20 +0200

    Sprawdzian

---
+++ main.py
@@ -0,0 +1,34 @@
+parzyste = 0
+tylesamo = 0
+iloscZer = 0
+iloscJedynek = 0
+koniecJeden = 0
+palindromy = 0
+
+with open('napisy.txt', 'r') as pliczek:
+    for linia in pliczek:
+        linia = linia.strip()
+        ciag = len(linia)
+
+    if ciag % 2 == 0:
+        parzyste += 1
+        print('Jest tyle parzystych napisów: ', parzyste, '\n')
+
+    if linia.count('0') == linia.count('1'):
+        tylesamo += 1
+        print('Jest tyle wyrazów mających tyle samo jedynek i zer:', tylesamo, '\n')
+
+    if linia.count('0'):
+        iloscZer = 1
+        print('Jest tyle wyrazów mających zera:', iloscZer, '\n')
+    elif linia.count('1'):
+        iloscJedynek += 1
+        print('Jest tyle wyrazów mających jedynki', iloscJedynek, '\n')
+
+    if linia.endswith('1'):
+        koniecJeden += 1
+        print('Jest tyle wyrazów kończączych się na jeden:', koniecJeden, '\n')
+
+    if linia == linia[::-1]:
+        palindromy += 1
+        print('Jest tyle palindromów:', palindromy)
